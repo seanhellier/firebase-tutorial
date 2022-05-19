746957298fe2efef7c0e7ae58453d41cc466a5c8
Sean Leif Hellier <sean@newtropolis.com>
2022-05-19 11:59:31 -0400

    put retrieved data on page

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+const cafeList = document.querySelector("#cafe-list");
+const form = document.querySelector("#add-cafe-form");
+
+// create element & render cafe
+function renderCafe(doc) {
+	let li = document.createElement("li");
+	let name = document.createElement("span");
+	let city = document.createElement("span");
+
+	li.setAttribute("data-id", doc.id);
+	name.textContent = doc.data().name;
+	city.textContent = doc.data().city;
+
+	li.appendChild(name);
+	li.appendChild(city);
+
+	cafeList.appendChild(li);
+}
+
 db.collection("cafes")
 	.get()
 	.then((snapshot) => {
 		snapshot.docs.forEach((doc) => {
-			console.log(doc.data());
+			renderCafe(doc);
 		});
 	});
