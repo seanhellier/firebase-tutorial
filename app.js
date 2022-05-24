d78e7225f4408eb55c5f547d6fd66fe40df54b43
Sean Leif Hellier <sean@newtropolis.com>
2022-05-24 11:52:04 -0400

    added automatic updating

--- app.js
+++ app.js
@@ -6,25 +6,35 @@ function renderCafe(doc) {
 	let li = document.createElement("li");
 	let name = document.createElement("span");
 	let city = document.createElement("span");
+	let cross = document.createElement("div");
 
 	li.setAttribute("data-id", doc.id);
 	name.textContent = doc.data().name;
 	city.textContent = doc.data().city;
+	cross.textContent = "x";
 
 	li.appendChild(name);
 	li.appendChild(city);
+	li.appendChild(cross);
 
 	cafeList.appendChild(li);
+
+	// deleting data
+	cross.addEventListener("click", (e) => {
+		e.stopPropagation();
+		let id = e.target.parentElement.getAttribute("data-id");
+		db.collection("cafes").doc(id).delete();
+	});
 }
 
-db.collection("cafes")
-	.get()
-	.then((snapshot) => {
-		snapshot.docs.forEach((doc) => {
-			renderCafe(doc);
-		});
-	});
+// getting data
+// db.collection('cafes').orderBy('city').get().then(snapshot => {
+//     snapshot.docs.forEach(doc => {
+//         renderCafe(doc);
+//     });
+// });
 
+// saving data
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	db.collection("cafes").add({
@@ -34,3 +44,19 @@ form.addEventListener("submit", (e) => {
 	form.name.value = "";
 	form.city.value = "";
 });
+
+// real-time listener
+db.collection("cafes")
+	.orderBy("name")
+	.onSnapshot((snapshot) => {
+		let changes = snapshot.docChanges();
+		changes.forEach((change) => {
+			console.log(change.doc.data());
+			if (change.type == "added") {
+				renderCafe(change.doc);
+			} else if (change.type == "removed") {
+				let li = cafeList.querySelector("[data-id=" + change.doc.id + "]");
+				cafeList.removeChild(li);
+			}
+		});
+	});
